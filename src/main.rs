f594fde5a85a73d5dd628608d541cee3afe6e3dc
Abhijeet Krishnan <abhijeet.krishnan@gmail.com>
2022-10-02 14:37:32 -0400

    Implement PPM creator

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+const IMAGE_WIDTH: u64 = 256;
+const IMAGE_HEIGHT: u64 = 256;
+
 fn main() {
-    println!("Hello, world!");
+    print!("P3\n{IMAGE_WIDTH} {IMAGE_HEIGHT}\n255\n");
+
+    for j in (0..IMAGE_HEIGHT).rev() {
+        for i in 0..IMAGE_WIDTH {
+            let r = (i as f64) / (IMAGE_WIDTH - 1) as f64;
+            let g = (j as f64) / (IMAGE_HEIGHT - 1) as f64;
+            let b = 0.25;
+
+            let ir = (255.999 * r) as i64;
+            let ig = (255.999 * g) as i64;
+            let ib = (255.999 * b) as i64;
+
+            print!("{ir} {ig} {ib}\n");
+        }
+    }
 }
