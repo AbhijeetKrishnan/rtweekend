6077d18fe18481d7feb0e429e085277f3a681084
Abhijeet Krishnan <abhijeet.krishnan@gmail.com>
2022-10-02 14:39:43 -0400

    Add progress indicator

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ fn main() {
     print!("P3\n{IMAGE_WIDTH} {IMAGE_HEIGHT}\n255\n");
 
     for j in (0..IMAGE_HEIGHT).rev() {
+        eprint!("\rScanlines remaining: {j} ");
         for i in 0..IMAGE_WIDTH {
             let r = (i as f64) / (IMAGE_WIDTH - 1) as f64;
             let g = (j as f64) / (IMAGE_HEIGHT - 1) as f64;
@@ -17,4 +18,5 @@ fn main() {
             print!("{ir} {ig} {ib}\n");
         }
     }
+    eprintln!("\nDone");
 }
